e9e390bd1e3d951194a92d16b1bfca62f1dec6f0
anne ganzeboom <anneganzeboom@gmail.com>
2018-08-20 17:08:01 +0200

    Add files via upload

--- milc2/js/main.js
+++ milc2/js/main.js
@@ -1,1 +1,10 @@
 console.log('hello world')
+// $("body").load(function() {
+//       console.log("adfasd")
+//      $('.slide-nav').addClass('animated slideInDown');
+// });
+function my_code(){
+$('.slide-nav').addClass('animated slideInDown');
+}
+
+window.onload=my_code();
