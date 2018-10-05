df8a7d14e18c003410e5987186e3196d1b49a06c
anne ganzeboom <anneganzeboom@gmail.com>
2018-10-05 14:50:23 +0200

    Add files via upload

--- banners/box-html/970x250-3/js/main.js
+++ banners/box-html/970x250-3/js/main.js
@@ -1,18 +1,18 @@
 window.onload = function () {
 
-var tl1 = new TimelineMax({repeat:1, repeatDelay:0.4})    
+var tl1 = new TimelineMax({repeat:1, repeatDelay:1.4})    
 tl1
 
-.to("#text1", 0.7, {autoAlpha: 1, left:"0px"})
-.to("#text1", 0.4,{delay:1.3, autoAlpha: 0, left:"-60px"})
+.to("#text1", 0.5, {autoAlpha: 1})
+.to("#text1", 0.4,{delay:1.5, autoAlpha: 0})
 .fromTo("#image2", 0.1, { visibility: "hidden", autoAlpha: 1}, { visibility: "visible", autoAlpha: 1},"second")
 
-.to("#text2", 0.7, {autoAlpha: 1, left:"0px"},"second")
-.to("#text2", 0.4, {delay:1.3,autoAlpha: 0, left:"-60px"})
+.to("#text2", 0.5, {autoAlpha: 1},"second")
+.to("#text2", 0.4, {delay:1.4,autoAlpha: 0})
 .fromTo("#endframe", 0.1, {visibility: "hidden", autoAlpha: 0}, {  visibility: "visible", autoAlpha: 1})
-.to("#text3", 0.5, {autoAlpha: 1, left:"0px"},"end")
-.to("#button", 0.5, {autoAlpha: 1, left:"0px"},"end")
-.fromTo("#button", 0.6, {scale:"0.98", autoAlpha: 1}, {scale:"1.02", autoAlpha: 1, yoyo:true, repeat:3 },"end")
+.to("#text3", 0.5, {autoAlpha: 1},"end")
+.to("#button", 0.5, {autoAlpha: 1},"end")
+.fromTo("#button", 0.5, {scale:"0.98", autoAlpha: 1}, {scale:"1.02", autoAlpha: 1, yoyo:true, repeat:3 },"end")
 
 
 
@@ -21,4 +21,3 @@ console.log(tl1.totalDuration());
 
 
 }
-
