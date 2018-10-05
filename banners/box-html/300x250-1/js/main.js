1a8d943b7e1f706f77df230252771c022340f895
anne ganzeboom <anneganzeboom@gmail.com>
2018-10-05 10:01:33 +0200

    Add files via upload

--- banners/box-html/300x250-1/js/main.js
+++ banners/box-html/300x250-1/js/main.js
@@ -3,8 +3,8 @@ window.onload = function () {
 var tl1 = new TimelineMax({repeat:1, repeatDelay:3})    
 tl1
 
-.fromTo("#image2", 0.1, { visibility: "hidden", autoAlpha: 0}, {delay:4, visibility: "visible", autoAlpha: 1})
-.fromTo("#image3", 0.1, { visibility: "hidden", autoAlpha: 0}, { delay:4, visibility: "visible", autoAlpha: 1})
+.fromTo("#image2", 0.1, { visibility: "hidden", autoAlpha: 0}, {delay:2.5, visibility: "visible", autoAlpha: 1})
+.fromTo("#image3", 0.1, { visibility: "hidden", autoAlpha: 0}, { delay:2.5, visibility: "visible", autoAlpha: 1})
 
 
 
--- banners/box-html/300x600/js/main.js
+++ banners/box-html/300x600/js/main.js
@@ -3,14 +3,14 @@ window.onload = function () {
 var tl1 = new TimelineMax({repeat:1, repeatDelay:3})    
 tl1
 
-.to("#text1", 1.2, {delay:0.2,autoAlpha: 1, left:"0px"})
-.to("#text1", 1.2,{delay:2.3, autoAlpha: 0, left:"-183px"})
-
+.to("#text1", 1, {autoAlpha: 1, left:"0px"})
+.to("#text1", 0.7,{delay:1.5, autoAlpha: 0, left:"-183px"})
 .fromTo("#image2", 0.1, { visibility: "hidden", autoAlpha: 1}, { visibility: "visible", autoAlpha: 1})
-.to("#text2", 1.2, {delay:0.2,autoAlpha: 1, left:"0px"})
-.to("#text2", 1.2, {delay:2.3,autoAlpha: 0, left:"-250px"})
+// .to("#image2", 0.2,{ autoAlpha: 1})
+.to("#text2", 1, {delay:0.2,autoAlpha: 1, left:"0px"})
+.to("#text2", 0.7, {delay:2,autoAlpha: 0, left:"-250px"})
 .fromTo("#endframe", 0.1, {visibility: "hidden", autoAlpha: 0}, {  visibility: "visible", autoAlpha: 1})
-.to("#text3", 1.2, {delay:0.2,autoAlpha: 1, left:"0px"})
+.to("#text3", 0.7, {autoAlpha: 1, left:"0px"},"end")
 .to("#button", 0.7, {autoAlpha: 1, left:"0px"})
 
 
@@ -20,4 +20,3 @@ console.log(tl1.totalDuration());
 
 
 }
-
